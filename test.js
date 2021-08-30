a1cdad5b895b59d69c425a6c52d2adde61f25403
Megan <smith.megan.design@gmail.com>
2021-08-30 16:26:03 -0600

    adding additional test

--- test.js
+++ test.js
@@ -3,3 +3,7 @@ const getGreeting = require('./project');
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+test('can create a first name last name greeting', ()=>{
+  expect(getGreeting("Marcy Smith")).toBe("Hi Marcy Smith")
+})
